12e301621d1a533dffd500834457d8e8c3c2e4f5
Santosh Varma Addala <126544248+santoshvarmaaddala@users.noreply.github.com>
2024-02-17 20:34:27 +0530

    Update gray.py

--- gray.py
+++ gray.py
@@ -1,5 +1,8 @@
 import cv2 as c
+#In imread func u declare the photo name or path to be convert
 img = c.imread(r"C:\Users\KITS\Pictures\virat.jpg")
-print(img)
+#Below line can be convert photo to Gray
+#If you want to Convert it into another color then choose another attribute
 gry = c.cvtColor(img,c.COLOR_BGR2GRAY)
+#In double quotes you have to give a name for converted image
 c.imwrite("gray.jpg",gry)
